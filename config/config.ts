abb07561bb5c05772f0b4273cc2e61a1c1b3a079
victor <robin_fc@qq.com>
2023-05-04 13:49:05 +0800

    chore: 首次进入网站，改为默认中文

--- config/config.ts
+++ config/config.ts
@@ -97,8 +97,8 @@ export default defineConfig({
    * @doc https://umijs.org/docs/max/i18n
    */
   locale: {
-    // default en-US
-    default: 'en-US',
+    // default zh-CN
+    default: 'zh-CN',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
